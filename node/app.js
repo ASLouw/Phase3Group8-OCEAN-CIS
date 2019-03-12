8bc670828624aaaeeaef84d1b0e2df17b001f04a
Brendan <brendanbath@gmail.com>
2019-03-12 23:07:22 +0200

    Change in layout
    Added new run script: npm run start

---
+++ node/app.js
@@ -0,0 +1,25 @@
+//dependencies
+const dbInfo = require("./local_modules/dbInfo");
+const clientInfo = new dbInfo();
+
+const app = async() => {
+    //let saveClient = await clientInfo.saveEntry(1,"Bob", "Doe", "SMS", 1,"qwerty", "0784693485", "bobDoe@gamil.com"
+    //);
+
+   // console.log("Saved client -->", saveClient);
+
+
+
+    let updateClient = await clientInfo.updateEntry("Jhon","Doe","Email","qwerty","0784693485","jhonDoe@gamil.com",1);
+
+    console.log("Updated client -->", updateClient);
+
+    let deleteClient = await clientInfo.deleteEntry(1);
+
+    console.log("Deleted client -->", deleteClient);
+
+    let clientList = await clientInfo.readEntries()
+    console.log("List -->",clientList);
+}
+
+app();

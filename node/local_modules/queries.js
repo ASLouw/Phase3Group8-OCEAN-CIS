e62bb11b4a94fba361c99b7e758aa8c28f8c589b
ASLouw <u15048366@tuks.co.za>
2019-03-14 18:41:19 +0200

    Update queries.js

--- node/local_modules/queries.js
+++ node/local_modules/queries.js
@@ -1,6 +1,6 @@
 module.exports = {
     insert_clientinfo: 'INSERT INTO clientinfo(client_id,client_name, client_surname, method_of_notification ,active, password, cell_number, email) VALUES (?,?,?,?,?,?.?,?)',
-    read_clientinfo: 'SELECT * FROM clientinfo',
+    get_ClientEmail: 'SELECT email FROM clientinfo WHERE clientinfo.client_id = ?',
     update_clientinfo: 'UPDATE clientinfo SET clientinfo.client_name = ?, clientinfo.client_surname = ?, clientinfo.method_of_notification = ?, clientinfo.password = ?, clientinfo.cell_number = ?, clientinfo.email = ? WHERE clientinfo.client_id = ?',   
     delete_clientinfo: 'UPDATE clientinfo SET clientinfo.active = 0 WHERE clientinfo.client_id = ?'
 }

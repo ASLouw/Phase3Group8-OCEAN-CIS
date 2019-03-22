3acf4ada5f48e48843fa2f54bd9cad7f0e5f46d6
ASLouw <u15048366@tuks.co.za>
2019-03-22 13:10:01 +0200

    Update crud.js
    Took out first connection

--- crud.js
+++ crud.js
@@ -4,14 +4,6 @@ var express=require('express');
 var app=express();
 var mysql = require('mysql2');
 
-var conn=mysql.createConnection(
-{
-	host : "eu-cdbr-west-02.cleardb.net",
-	user : "bdffef71b5c89d",
-	password : "6e8120b4",
-	database : "heroku_e0c1ec409484908"
-});
-
 function onRequest(request, response) 
 {
 	var _url = url.parse(request.url, true);

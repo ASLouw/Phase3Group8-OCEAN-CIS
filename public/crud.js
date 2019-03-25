180cf511bc1cadf393e0b568c4198fa7242c8d5c
Andreas <u15048366A@tuks.co.za>
2019-03-25 12:31:22 +0200

    Added package file and edidted crud
    Crud now connects to live database

--- public/crud.js
+++ public/crud.js
@@ -45,21 +45,21 @@ app.get('/reactivate', function (req, res)
 
 app.get('/insertInfo', function (req, res)
 {
-    var con = sql.createConnection(
+  /*  var con = sql.createConnection(
     {
       host: "localhost",
       user: "root",
       password: "",
       database: "u17140634_cos301_client_information_database"
-    });
+    });*/
 
-    /*var conn=mysql.createConnection(
+    var con=sql.createConnection(
 	{
 		host : "eu-cdbr-west-02.cleardb.net",
 		user : "bdffef71b5c89d",
 		password : "6e8120b4",
 		database : "heroku_e0c1ec409484908"
-	});*/
+	});
 
     var sqlQ = "INSERT INTO clientinfo (client_name, client_surname, method_of_notification, active, cell_number, email, home_address) VALUES('"+req.query.clName+"','"+req.query.clSName+"','"+req.query.MoN+"', 1 ,'"+req.query.cellNum+"','"+req.query.email+"','"+req.query.address+"')";
 
@@ -81,21 +81,21 @@ app.get('/insertInfo', function (req, res)
 
 app.get('/updateinfo', function (req, res)
 {
-    var con = sql.createConnection(
+   /* var con = sql.createConnection(
     {
       host: "localhost",
       user: "root",
       password: "",
       database: "u17140634_cos301_client_information_database"
-    });
+    });*/
 
-    /*var conn=mysql.createConnection(
+    var con=sql.createConnection(
 	{
 		host : "eu-cdbr-west-02.cleardb.net",
 		user : "bdffef71b5c89d",
 		password : "6e8120b4",
 		database : "heroku_e0c1ec409484908"
-	});*/
+	});
 
     var sqlQ = "SELECT * FROM clientinfo WHERE client_id = "+req.query.clID;
 
@@ -147,21 +147,21 @@ app.get('/updateinfo', function (req, res)
 			addr = req.query.address;
 		}
 
-/*		var conn = mysql.createConnection(
+		var conn = sql.createConnection(
 		{
 			host : "eu-cdbr-west-02.cleardb.net",
 			user : "bdffef71b5c89d",
 			password : "6e8120b4",
 			database : "heroku_e0c1ec409484908"
 		});
-*/
-		var conn = sql.createConnection(
+
+		/*var conn = sql.createConnection(
 	    {
 	      host: "localhost",
 	      user: "root",
 	      password: "",
 	      database: "u17140634_cos301_client_information_database"
-	    });
+	    });*/
 
 		var sqlQ = "UPDATE clientinfo SET clientinfo.client_name = '"+ name +"', clientinfo.client_surname = '"+ sName +"', clientinfo.method_of_notification = '"+ MoN +"', clientinfo.cell_number = '"+ cN +"', clientinfo.email = '"+ Em +"', clientinfo.home_address = '"+ addr +"' WHERE clientinfo.client_id = "+req.query.clID;
 
@@ -184,21 +184,21 @@ app.get('/updateinfo', function (req, res)
 
 app.get('/deleteinfo', function (req, res)
 {
-    var con = sql.createConnection(
+    /*var con = sql.createConnection(
     {
       host: "localhost",
       user: "root",
       password: "",
       database: "u17140634_cos301_client_information_database"
-    });
+    });*/
 
-    /*var conn=mysql.createConnection(
+    var con=sql.createConnection(
 	{
 		host : "eu-cdbr-west-02.cleardb.net",
 		user : "bdffef71b5c89d",
 		password : "6e8120b4",
 		database : "heroku_e0c1ec409484908"
-	});*/
+	});
 
     var sqlQ = "UPDATE clientinfo SET  clientinfo.active = 0 WHERE clientinfo.client_id = "+req.query.clID;
 
@@ -220,21 +220,21 @@ app.get('/deleteinfo', function (req, res)
 
 app.get('/reactivateInfo', function (req, res)
 {
-    var con = sql.createConnection(
+   /* var con = sql.createConnection(
     {
       host: "localhost",
       user: "root",
       password: "",
       database: "u17140634_cos301_client_information_database"
-    });
+    });*/
 
-    /*var conn=mysql.createConnection(
+    var con=sql.createConnection(
 	{
 		host : "eu-cdbr-west-02.cleardb.net",
 		user : "bdffef71b5c89d",
 		password : "6e8120b4",
 		database : "heroku_e0c1ec409484908"
-	});*/
+	});
 
     var sqlQ = "UPDATE clientinfo SET  clientinfo.active = 1 WHERE clientinfo.client_id = "+req.query.clID;
 
@@ -258,21 +258,21 @@ app.set('view engine', 'ejs');
 
 app.get('/searchInfo', function(req,res) 
 {
-    var con = sql.createConnection(
+   /* var con = sql.createConnection(
     {
       host: "localhost",
       user: "root",
       password: "",
       database: "u17140634_cos301_client_information_database"
-    });
+    });*/
 
-    /*var conn=mysql.createConnection(
+    var con=sql.createConnection(
 	{
 		host : "eu-cdbr-west-02.cleardb.net",
 		user : "bdffef71b5c89d",
 		password : "6e8120b4",
 		database : "heroku_e0c1ec409484908"
-	});*/
+	});
 
     var query = "SELECT * FROM clientinfo WHERE client_id = "+req.query.clID;
 
@@ -292,21 +292,21 @@ app.get('/searchInfo', function(req,res)
 
 app.get('/display', function(req,res) 
 {
-    var con = sql.createConnection(
+    /*var con = sql.createConnection(
     {
       host: "localhost",
       user: "root",
       password: "",
       database: "u17140634_cos301_client_information_database"
-    });
+    });*/
 
-    /*var conn=mysql.createConnection(
+    var con=sql.createConnection(
 	{
 		host : "eu-cdbr-west-02.cleardb.net",
 		user : "bdffef71b5c89d",
 		password : "6e8120b4",
 		database : "heroku_e0c1ec409484908"
-	});*/
+	});
 
     var query = "SELECT * FROM clientinfo";
 

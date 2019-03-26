b954a3c83d1c22054a88beac63ea596e804df68d
Andreas <u15048366A@tuks.co.za>
2019-03-26 11:12:55 +0200

    Added dbcon, queries, logs .js and modiefied clients.js

---
+++ local_modules/queries.js
@@ -0,0 +1,8 @@
+module.exports = {
+    insert_clientinfo: 'INSERT INTO clientinfo(client_id,client_name, client_surname, method_of_notification ,active, password, cell_number, email) VALUES (?,?,?,?,?,?.?,?)',
+    get_ClientEmail: 'SELECT email FROM clientinfo WHERE clientinfo.client_id = ?',
+    update_clientinfo: 'UPDATE clientinfo SET clientinfo.client_name = ?, clientinfo.client_surname = ?, clientinfo.method_of_notification = ?, clientinfo.password = ?, clientinfo.cell_number = ?, clientinfo.email = ? WHERE clientinfo.client_id = ?',   
+    delete_clientinfo: 'UPDATE clientinfo SET clientinfo.active = 0 WHERE clientinfo.client_id = ?',
+    get_Logs: "SELECT * FROM transactions WHERE timestamp >= ? AND timestamp <= ? AND client_id = ?",
+    get_cleintActive: "SELECT active FROM clientinfo WHERE clientinfo.client_id = ?"
+}

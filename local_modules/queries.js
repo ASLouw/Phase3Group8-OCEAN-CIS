fc72cc52711777210bda0c43e30ea9fa985adf26
Andreas <u15048366A@tuks.co.za>
2019-04-04 16:24:36 +0200

    Update queries.js

--- local_modules/queries.js
+++ local_modules/queries.js
@@ -12,5 +12,5 @@ module.exports = {
     get_subscriptions: "SELECT * FROM subscriptions",
     update_subscription:"UPDATE subscriptions SET subscriptions.url = ? WHERE subscriptions.subsystem = ?",
     delete_logs:"DELETE FROM transactions",
-    get_1000_logs:"SELECT top 1000 * FROM transactions"
+    get_1000_logs:"SELECT * FROM transactions limit 1000"
 }
